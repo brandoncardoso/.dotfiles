79d0fb8921a5a27ec948b8cd10458d9c6287170e
Brandon Cardoso <1226170+BrandonCardoso@users.noreply.github.com>
2021-04-06 14:27:26 -0400

    adjust bspwm & polybar spacing/colours

--- .config/polybar/scripts/spotify_status.sh
+++ .config/polybar/scripts/spotify_status.sh
@@ -1,141 +1,58 @@
-#!/usr/bin/env python
+#!/bin/bash
 
-import sys
-import dbus
-import argparse
+# The name of polybar bar which houses the main spotify module and the control modules.
+PARENT_BAR="bottom"
+PARENT_BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '-t',
-    '--trunclen',
-    type=int,
-    metavar='trunclen'
-)
-parser.add_argument(
-    '-f',
-    '--format',
-    type=str,
-    metavar='custom format',
-    dest='custom_format'
-)
-parser.add_argument(
-    '-p',
-    '--playpause',
-    type=str,
-    metavar='play-pause indicator',
-    dest='play_pause'
-)
-parser.add_argument(
-    '--font',
-    type=str,
-    metavar='the index of the font to use for the main label',
-    dest='font'
-)
-parser.add_argument(
-    '--playpause-font',
-    type=str,
-    metavar='the index of the font to use to display the playpause indicator',
-    dest='play_pause_font'
-)
-parser.add_argument(
-    '-q',
-    '--quiet',
-    action='store_true',
-    help="if set, don't show any output when the current song is paused",
-    dest='quiet',
-)
+# Set the source audio player here.
+# Players supporting the MPRIS spec are supported.
+# Examples: spotify, vlc, chrome, mpv and others.
+# Use `playerctld` to always detect the latest player.
+# See more here: https://github.com/altdesktop/playerctl/#selecting-players-to-control
+PLAYER="spotify"
 
-args = parser.parse_args()
+# Format of the information displayed
+# Eg. {{ artist }} - {{ album }} - {{ title }}
+# See more attributes here: https://github.com/altdesktop/playerctl/#printing-properties-and-metadata
+FORMAT="{{ title }} - {{ artist }}"
 
+# Sends $2 as message to all polybar PIDs that are part of $1
+update_hooks() {
+    while IFS= read -r id
+    do
+        polybar-msg -p "$id" hook spotify-play-pause "$2" 1>/dev/null 2>&1
+    done < <(echo "$1")
+}
 
-def fix_string(string):
-    # corrects encoding for the python version used
-    if sys.version_info.major == 3:
-        return string
-    else:
-        return string.encode('utf-8')
+PLAYERCTL_STATUS=$(playerctl --player=$PLAYER status 2>/dev/null)
+EXIT_CODE=$?
 
+if [ $EXIT_CODE -eq 0 ]; then
+    STATUS=$PLAYERCTL_STATUS
+	polybar-msg -p "$PARENT_BAR_PID" cmd show.spotify > /dev/null 2>&1
+	polybar-msg -p "$PARENT_BAR_PID" cmd show.spotify-next > /dev/null 2>&1
+	polybar-msg -p "$PARENT_BAR_PID" cmd show.spotify-prev > /dev/null 2>&1
+	polybar-msg -p "$PARENT_BAR_PID" cmd show.spotify-play-pause > /dev/null 2>&1
+else
+    STATUS="No player is running"
+	polybar-msg -p "$PARENT_BAR_PID" cmd hide.spotify > /dev/null 2>&1
+	polybar-msg -p "$PARENT_BAR_PID" cmd hide.spotify-next > /dev/null 2>&1
+	polybar-msg -p "$PARENT_BAR_PID" cmd hide.spotify-prev > /dev/null 2>&1
+	polybar-msg -p "$PARENT_BAR_PID" cmd hide.spotify-play-pause > /dev/null 2>&1
+fi
 
-def truncate(name, trunclen):
-    if len(name) > trunclen:
-        name = name[:trunclen]
-        name += '...'
-        if ('(' in name) and (')' not in name):
-            name += ')'
-    return name
-
-
-
-# Default parameters
-output = fix_string(u'{play_pause} {artist}: {song}')
-trunclen = 35
-play_pause = fix_string(u'\u25B6,\u23F8') # first character is play, second is paused
-
-label_with_font = '%{{T{font}}}{label}%{{T-}}'
-font = args.font
-play_pause_font = args.play_pause_font
-
-quiet = args.quiet
-
-# parameters can be overwritten by args
-if args.trunclen is not None:
-    trunclen = args.trunclen
-if args.custom_format is not None:
-    output = args.custom_format
-if args.play_pause is not None:
-    play_pause = args.play_pause
-
-try:
-    session_bus = dbus.SessionBus()
-    spotify_bus = session_bus.get_object(
-        'org.mpris.MediaPlayer2.spotify',
-        '/org/mpris/MediaPlayer2'
-    )
-
-    spotify_properties = dbus.Interface(
-        spotify_bus,
-        'org.freedesktop.DBus.Properties'
-    )
-
-    metadata = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
-    status = spotify_properties.Get('org.mpris.MediaPlayer2.Player', 'PlaybackStatus')
-
-    # Handle play/pause label
-
-    play_pause = play_pause.split(',')
-
-    if status == 'Playing':
-        play_pause = play_pause[0]
-    elif status == 'Paused':
-        play_pause = play_pause[1]
-    else:
-        play_pause = str()
-
-    if play_pause_font:
-        play_pause = label_with_font.format(font=play_pause_font, label=play_pause)
-
-    # Handle main label
-
-    artist = fix_string(metadata['xesam:artist'][0]) if metadata['xesam:artist'] else ''
-    song = fix_string(metadata['xesam:title']) if metadata['xesam:title'] else ''
-    album = fix_string(metadata['xesam:album']) if metadata['xesam:album'] else ''
-
-    if (quiet and status == 'Paused') or (not artist and not song and not album):
-        print('')
-    else:
-        if font:
-            artist = label_with_font.format(font=font, label=artist)
-            song = label_with_font.format(font=font, label=song)
-            album = label_with_font.format(font=font, label=album)
-
-        # Add 4 to trunclen to account for status symbol, spaces, and other padding characters
-        print(truncate(output.format(artist=artist,
-                                     song=song,
-                                     play_pause=play_pause,
-                                     album=album), trunclen + 4))
-
-except Exception as e:
-    if isinstance(e, dbus.exceptions.DBusException):
-        print('')
-    else:
-        print(e)
+if [ "$1" == "--status" ]; then
+    echo "$STATUS"
+else
+    if [ "$STATUS" = "Stopped" ]; then
+        echo "No music is playing"
+    elif [ "$STATUS" = "Paused"  ]; then
+        update_hooks "$PARENT_BAR_PID" 2
+        playerctl --player=$PLAYER metadata --format "$FORMAT"
+    elif [ "$STATUS" = "No player is running"  ]; then
+        echo "$STATUS"
+    else
+        update_hooks "$PARENT_BAR_PID" 1
+        playerctl --player=$PLAYER metadata --format "$FORMAT"
+    fi
+fi
