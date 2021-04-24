c14f227aee80666334bd17699dbab92f3c12cff0
Brandon Cardoso <1226170+BrandonCardoso@users.noreply.github.com>
2021-04-24 18:37:51 -0400

    separate bspc rules

---
+++ bspwm/.config/bspwm/bspc_rules.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+bspc rule -a Yad state=floating layer=above
+bspc rule -a Steam state=floating focus=off
+bspc rule -a mpv state=floating
+bspc rule -a discord desktop=^4
+bspc rule -a qBittorrent state=floating desktop=^5
+bspc rule -a Pavucontrol state=floating
+bspc rule -a Org.gnome.Nautilus state=floating
+bspc rule -a Pcmanfm state=floating
+
+steamlib=~/.local/share/Steam    # path to your steamlibrary
+mygames="$(ls $steamlib/steamapps/appmanifest_*.acf | sed 's/[^0-9]*//g')"    # this sed command removes everything but the digits
+
+for game in $mygames; do
+    bspc rule -a steam_app_$game state=fullscreen layer=above
+done
+
+# scratchpad
+bspc rule -a "Alacritty:scratchpad" sticky=on state=floating hidden=on
+~/.config/bspwm/start_scratch.sh
