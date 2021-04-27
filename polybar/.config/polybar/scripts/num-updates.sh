e10b9ef430bb1633cb9a8a9e7fef5371decf2345
Brandon Cardoso <1226170+BrandonCardoso@users.noreply.github.com>
2021-04-27 17:04:08 -0400

    move polybar update module, hide when 0 updates

--- polybar/.config/polybar/scripts/num-updates.sh
+++ polybar/.config/polybar/scripts/num-updates.sh
@@ -5,6 +5,4 @@ numUpdates=$(apt-get -s -o Debug::NoLocking=true upgrade | grep ^Inst | wc -l)
 
 if [[ numUpdates -gt 0 ]]; then
 	echo "%{F#50fa7b}$icon $numUpdates%{F-}"
-else
-	echo "%{F#6272a4}$icon $numUpdates%{F-}"
 fi
