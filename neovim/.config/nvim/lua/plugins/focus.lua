e2870a8d20dec871efa619f09d4649ebf638e22b
Brandon Cardoso <1226170+brandoncardoso@users.noreply.github.com>
2025-07-06 17:10:16 +0900

    disable split autoresize

--- neovim/.config/nvim/lua/plugins/focus.lua
+++ neovim/.config/nvim/lua/plugins/focus.lua
@@ -4,10 +4,14 @@ return {
 		version = '*',
 		lazy = false,
 		config = function()
-			require("focus").setup({})
+			require("focus").setup({
+				autoresize = {
+					enable = false,
+				}
+			})
 
 			local focusmap = function(direction)
-				vim.keymap.set('n', '<Leader>'..direction, function()
+				vim.keymap.set('n', '<Leader>' .. direction, function()
 					require('focus').split_command(direction)
 				end, {
 					desc = string.format('Create or move to split (%s)', direction),
