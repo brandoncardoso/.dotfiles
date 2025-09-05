0953d87c4db2faa865ffa3b1dc7213a983e17072
Brandon Cardoso <1226170+brandoncardoso@users.noreply.github.com>
2025-09-05 15:52:38 +0900

    add gdscript + c# lsp, godot project detection

--- neovim/.config/nvim/lua/plugins/nvim-lsp.lua
+++ neovim/.config/nvim/lua/plugins/nvim-lsp.lua
@@ -12,6 +12,7 @@ return {
 			'hrsh7th/nvim-cmp',
 			'l3mon4d3/luasnip',
 			'saadparwaiz1/cmp_luasnip',
+			'hoffs/omnisharp-extended-lsp.nvim',
 		},
 		config = function()
 			local cmp = require('cmp')
@@ -27,12 +28,16 @@ return {
 			)
 
 			require('mason').setup()
+
+			require('lspconfig').gdscript.setup({})
+
 			require('mason-lspconfig').setup({
 				ensure_installed = {
 					'lua_ls',
 					'ts_ls', -- typescript
 					'gopls',
 					'rust_analyzer',
+					'omnisharp',
 				},
 				handlers = {
 					function(server)
@@ -51,7 +56,50 @@ return {
 							}
 						})
 					end,
-				},
+					["omnisharp"] = function()
+						require("lspconfig").omnisharp.setup({
+							handlers = {
+								["textDocument/definition"] = require("omnisharp_extended").handler,
+							},
+							-- cmd = { "dotnet", "/path/to/omnisharp/OmniSharp.dll" },
+
+							-- Enables support for reading code style, naming convention and analyzer
+							-- settings from .editorconfig.
+							enable_editorconfig_support = true,
+
+							-- If true, MSBuild project system will only load projects for files that
+							-- were opened in the editor. This setting is useful for big C# codebases
+							-- and allows for faster initialization of code navigation features only
+							-- for projects that are relevant to code that is being edited. With this
+							-- setting enabled OmniSharp may load fewer projects and may thus display
+							-- incomplete reference lists for symbols.
+							enable_ms_build_load_projects_on_demand = false, -- default false
+
+							-- Enables support for roslyn analyzers, code fixes and rulesets.
+							enable_roslyn_analyzers = true, -- default false
+
+							-- Specifies whether 'using' directives should be grouped and sorted during
+							-- document formatting.
+							organize_imports_on_format = true, -- default false
+
+							-- Enables support for showing unimported types and unimported extension
+							-- methods in completion lists. When committed, the appropriate using
+							-- directive will be added at the top of the current file. This option can
+							-- have a negative impact on initial completion responsiveness,
+							-- particularly for the first few completion sessions after opening a
+							-- solution.
+							enable_import_completion = true, -- default false
+
+							-- Specifies whether to include preview versions of the .NET SDK when
+							-- determining which version to use for project loading.
+							sdk_include_prereleases = true,
+
+							-- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+							-- true
+							analyze_open_documents_only = true, -- default false
+						})
+					end,
+				}
 			})
 
 			local cmp_select = { behavior = cmp.SelectBehavior.Select }
@@ -108,8 +156,8 @@ return {
 					{ name = 'nvim_lsp' },
 					{ name = 'luasnip' },
 				}, {
-					{ name = 'buffer' },
-				}),
+						{ name = 'buffer' },
+					}),
 			})
 		end,
 	}
