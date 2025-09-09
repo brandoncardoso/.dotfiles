69b8a6f1ce4a735481c6346787db3443cf529476
Brandon Cardoso <1226170+brandoncardoso@users.noreply.github.com>
2025-09-09 18:39:31 +0900

    remove old/unused configs

--- bspwm/.config/bspwm/start_scratch.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-## check if scratchpad is already running, launch if not
-if [ "$(ps -x | grep -c 'scratchpad')" -eq "1" ]; then
-    alacritty --class scratchpad --title scratchpad -e ~/.config/bspwm/scratchpad.sh
-fi
--- polybar/.config/polybar/scripts/num-updates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-icon="%{T3}%{T}"
-numUpdates=$(apt-get -s -o Debug::NoLocking=true upgrade | grep ^Inst | wc -l)
-
-if [[ numUpdates -gt 0 ]]; then
-	echo "%{F#50fa7b}$icon $numUpdates%{F-}"
-fi
