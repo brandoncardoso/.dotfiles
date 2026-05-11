2cc325494a0e8161240c30488f22dcbc5fba7d6c
Brandon Cardoso <1226170+brandoncardoso@users.noreply.github.com>
2026-05-12 00:18:08 +0900

    fix starship always prepending newline
    was causing permanent newline above first prompt

--- fish/.config/fish/conf.d/starship.fish
+++ fish/.config/fish/conf.d/starship.fish
@@ -1,3 +1,13 @@
+set -g __starship_first_prompt 1
+
+function __starship_newline --on-event fish_prompt
+    if test "$__starship_first_prompt" = 1
+        set __starship_first_prompt 0
+    else
+        echo
+    end
+end
+
 if command -q starship
-	starship init fish | source
+    starship init fish | source
 end
